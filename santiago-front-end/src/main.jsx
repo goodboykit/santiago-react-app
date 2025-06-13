635709f2110040d97c783618c0321de46c1d42b5
goodboykit <santiagogoodboykit@gmail.com>
2025-06-13 13:17:23 +0800

    commit now

--- santiago-front-end/src/main.jsx
+++ santiago-front-end/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/global.css'; // Import global CSS first to establish base styles
 import './App.css'; 
 import './styles/Layout.css';
 import './styles/Navbar.css';
